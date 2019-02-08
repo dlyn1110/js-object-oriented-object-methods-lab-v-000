88482e606d6ef704675ffe763803f5df010291a5
Dani Lyn <dlyn1110@gmail.com>
2019-02-08 19:22:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function BoardMember (name, homeState, training) {
   this.homeState = homeState;
   this.training = training ;
   this.veto = function(veto) {
-    return "No, I must Disagree";
+    return "No, I must disagree";
   }
   this.appprove = function(approve){
     return "You can do that!";
