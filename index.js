a5109206a6de481d4f5860bef95ac955b63b2f2d
Dani Lyn <dlyn1110@gmail.com>
2019-02-08 19:18:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-function BoardMember (name, state,)
+function BoardMember (name, homeState, training) {
+  this.name = name ;
+  this.homeState = homeState;
+  this.training = training ;
+  this.veto = function(veto) {
+    return "No, I must Dosagree";
+  }
+  this.appprove = function(approve){
+    return "You can do that";
+  }
+  this.doCharity = function(doCharity) {
+    return "I like to help peopl";
+  }
+  this.releasePressStatement = function(releasePressStatement) {
+    return "You will see great things from Scuber" ;
+  }
+}
